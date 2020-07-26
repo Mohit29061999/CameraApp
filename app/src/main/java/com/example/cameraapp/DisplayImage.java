c72715cf3855514a2432aea4eb29eea62c13aac9
Mohit Pandey <Pandeymohit348@gmail.com>
2020-07-26 10:31:52 +0530

    Add existing file

---
+++ app/src/main/java/com/example/cameraapp/DisplayImage.java
@@ -0,0 +1,20 @@
+package com.example.cameraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class DisplayImage extends AppCompatActivity {
+ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_image);
+        imageView = findViewById(R.id.mimageView);
+        Bitmap bitmap = BitmapFactory.decodeFile(getIntent().getStringExtra("image_path"));
+        imageView.setImageBitmap(bitmap);
+    }
+}
